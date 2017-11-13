103410da7c087eba680722b09715f369b9292b0a
wangrui <35493032@qq.com>
2017-11-13 15:47:39 +0800

    -i 后面可以跟多个文件名

--- NLP/mergewords.py
+++ NLP/mergewords.py
@@ -1,4 +1,3 @@
-# #!/usr/bin/python
 # -*- coding:utf-8 -*-
 #
 # 作者:王睿
@@ -69,42 +68,54 @@ def GetStopWords(listOfFileName, FileName='words.txt'):
 if __name__ == '__main__':
     usage = """
     主要用来合并字典文件，停词字典，关键词字典.....
-    MergeWrods.py -i 字典文件名1 -o 合并后的文件
+    mergewords.py -i 字典文件名1 -o 合并后的文件
 
-    -i files        要合并的字典文件，支持通配符
+    -i files        要合并的字典文件，支持?,*通配符
     -o file         保存成文件，不写文件名用默认文件名，默认文件是words.txt
 
 
-    MergeWrods.py  -i s*.txt -o ~/stopword.txt
-    合并/stop目录下s*.txt的所有文件，合并后的文件是~/stopword.txt
+    mergewords.py  -i s*.txt t*.txt -o ~/stopword.txt
+    合并/stop目录下s*.txt和t*.txt的所有文件，合并后的文件是~/stopword.txt
 
-    MergeWrods.py /stop/*  -o ~/stopword.txt
+    mergewords.py /stop/*  -o ~/stopword.txt
     合并/stop目录下的所有文件，合并后的文件是~/stopword.txt
+
+    python mergewords.py -i words/CNstopwords.txt words/stop*.txt -o words/last_words_cn.txt
+    python mergewords.py -i words/CNstopwords.txt*.txt -o words/last_words_cn.txt
     """
 
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "i:o:", ["in=", "out="])
-    except getopt.GetoptError:
-        # print help information and exit:
+    if len(sys.argv[1:]) == 0:
         print(usage)
         exit()
-    if len(opts) == 0:
-        print(usage)
-        exit()
+    opts=[]
+    curopt = 0
+    for arg in sys.argv[1:]:
+        if arg[:1] == '-':
+            if curopt !=0:
+                opts.append(curopt)
+            curopt={'key':arg,'val':[]}
+        else:
+            curopt['val'].append(arg)
+    opts.append(curopt)
     infile = 0
     outfile = 'words.txt'
-    # dir = './'
-    for k, v in opts:
-        if k in ('-o', '--out'):
-            outfile = v.strip()
-        elif k in ('-i', '--in'):
-            infile = v.strip()
-        elif k in ('-h', '--help'):
+    for opt in opts:
+        if opt['key'] in ('-o'):
+            outfile = opt['val'][0].strip()
+        elif opt['key'] in ('-i', '--in'):
+            infile = opt['val']
+        elif opt['key'] in ('-h', '--help'):
             print(usage)
             exit()
-    print('in=%s,out=%s' % (infile, outfile))
+    # print('in=%s,out=%s' % (infile, outfile))
     if infile is not 0:
-        files = [f for f in glob.glob(infile)]
+        files = []
+        for inf in infile:
+            if '*' in inf or '?' in inf:
+                fs = [f for f in glob.glob(inf)]
+                files.extend(fs)
+            else:
+                files.append(inf)
         GetStopWords(files,outfile)
     else:
         print(usage)
