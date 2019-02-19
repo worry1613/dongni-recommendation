fab44c68fa81aab76fef638df13c5ff3693d020a
wangrui <35493032@qq.com>
2019-02-19 13:20:20 +0800

    代码重构，写成类形式

---
+++ mining/nlp/NLP.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @创建时间 : 19/2/2018
+# @作者    : worry1613(549145583@qq.com)
+# GitHub  : https://github.com/worry1613
+# @CSDN   : http://blog.csdn.net/worryabout/
+
+
+class NLP:
+    """
+    基类
+    """
+    pass
+
+
