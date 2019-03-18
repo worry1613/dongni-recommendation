08c1dbab915b16f2de8bb9016123a9e38179a37f
wangrui <35493032@qq.com>
2019-03-18 12:45:34 +0800

    处理组织机构名

--- mining/ner/corpus.py
+++ mining/ner/corpus.py
@@ -6,13 +6,15 @@
 from config import get_config
 from util import q_to_b
 import os, sys
+
 reload(sys)
 sys.setdefaultencoding('utf8')
 import logging
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-class Corpus:
 
+
+class Corpus:
     _maps = {u't': u'TIME',  # 时间
              u'nr': u'PER',  # 人
              u'ns': u'ORG',  # 组织
@@ -21,14 +23,14 @@ class Corpus:
     def __init__(self):
         pass
 
-    def pre_process(self,fin,fout):
+    def pre_process(self, fin, fout):
         lines = self.load_corpus(fin)
         lines_new = []
         for line in lines:
-            words = q_to_b(line.decode('utf-8').strip()).split('  ')[1:]    #全角转半角
-            new_words = self.process_time(words)                        #处理时间
-            new_words = self.process_person(new_words)                  #处理人名
-            # new_words = self.process_org(new_words)                     #处理组织机构
+            words = q_to_b(line.decode('utf-8').strip()).split('  ')[1:]  # 全角转半角
+            new_words = self.process_time(words)  # 处理时间
+            new_words = self.process_person(new_words)  # 处理人名
+            new_words = self.process_org(new_words)                     #处理组织机构
             lines_new.append('  '.join(new_words))
         self.save_corpus(data='\n'.join(lines_new).encode('utf-8'), file_path=fout)
 
@@ -75,18 +77,16 @@ class Corpus:
         temp = ''
         lw = len(words)
         index = 0
-        logging.info(words)
         while index < lw:
             word = words[index]
-            logging.info('index='+str(index))
-            if '/nr' in word and index+1< lw and '/nr' in words[index+1]:
-                temp = word[:-3] + words[index+1]
+            if '/nr' in word and index + 1 < lw and '/nr' in words[index + 1]:
+                temp = word[:-3] + words[index + 1]
                 new_words.append(temp)
                 temp = ''
                 index = index + 1
             else:
                 new_words.append(word)
-            index = index+1
+            index = index + 1
         return new_words
 
     def process_org(self, words):
@@ -96,12 +96,16 @@ class Corpus:
         new_words = []
         temp = ''
         for k, word in enumerate(words):
-            if '/t' in word:
-                temp = temp[:-3] + word
-            elif temp:
+            if '[' in word:
+                temp = word.split('/')[0][1:]
+            elif ']' in word and temp:
+                w = word.split('/')[0]
+                pos = word.split(']')[1]
+                temp += w + '/' + pos
                 new_words.append(temp)
                 temp = ''
-                new_words.append(word)
+            elif temp:
+                temp += word.split('/')[0]
             else:
                 new_words.append(word)
 
@@ -110,4 +114,4 @@ class Corpus:
 
 if __name__ == '__main__':
     corpus = Corpus()
-    corpus.pre_process('data/rmrb199801.txt','data/rmrb.txt')
+    corpus.pre_process('data/rmrb199801.txt', 'data/rmrb.txt')
