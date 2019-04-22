c29120ef0754b9b77b675b6330996a21114131de
wangrui <35493032@qq.com>
2019-04-22 14:42:22 +0800

    修改成命令行操作，不用每次传参再改代码 功能更加单一，只生成w2v 模型文件

--- mining/nlp/words2v.py
+++ mining/nlp/words2v.py
@@ -8,6 +8,8 @@ import os,sys
 
 from NLP import nlp
 
+from optparse import OptionParser
+
 reload(sys)
 sys.setdefaultencoding('utf8')
 from gensim.models import word2vec
@@ -24,7 +26,10 @@ class W2v(nlp):
     def load(self,fmodel):
         self.model = word2vec.Word2Vec.load(fmodel)
 
-    def process(self,size=100,window=5,min_count=5,workers=8):
+    def process(self,size=100,window=5,min_count=5):
+        import multiprocessing
+        cpu_count = multiprocessing.cpu_count()
+        workers = cpu_count
         sentences = word2vec.LineSentence(self.file)
         self.model = word2vec.Word2Vec(sentences,size=size,window=window,min_count=min_count,workers=workers)
 
@@ -37,31 +42,39 @@ class W2v(nlp):
 
 
 if __name__ == '__main__':
-    file_txt = '/Users/wangrui/百度云同步盘/中文语料库/THUCNews/last_THUCNews_w2v.txt'
-    file_model_64 = '/Users/wangrui/百度云同步盘/中文语料库/THUCNews/last_THUCNews_gensim_w2v.bin'
-    file_model_200 = '/Users/wangrui/百度云同步盘/中文语料库/THUCNews/last_THUCNews_gensim_w2v_200.bin'
-    w = W2v(file_txt)
+
+    parser = OptionParser()
+    parser.add_option('-f', '--file', type=str,  help='已经分了词的语料库文件', dest='wordsfile')
+    parser.add_option('-m', '--model', type=str,  help='训练完成了w2v模型文件', dest='modelfile')
+    parser.add_option('-s', '--size', type=int, default=100, help='w2v向量维度')
+
+    options, args = parser.parse_args()
+    if not (options.wordsfile or options.modelfile):
+        parser.print_help()
+        exit()
+
+    w = W2v(options.wordsfile)
     logging.info('<<<<<<<<<<<<<<<<')
-    w.process(size=10)
+    w.process()
     logging.info('>>>>>>>>>>>>>>>>')
-    w.save('.//last_THUCNews_gensim_w2v_10.bin')
+    w.save(options.modelfile)
 
 
-    print '"家装" 和 "家居" 的相似度:' + str(w.model.similarity(u'家装', u'家居'))
+    # print '"家装" 和 "家居" 的相似度:' + str(w.model.similarity(u'家装', u'家居'))
+    #
+    # # 计算某个词的相关词列表
+    # y2 = w.model.most_similar(u'家居', topn=20)  # 20个最相关的
+    # for item in y2:
+    #     print item[0], item[1]
+    # print "--------\n"
 
-    # 计算某个词的相关词列表
-    y2 = w.model.most_similar(u'家居', topn=20)  # 20个最相关的
-    for item in y2:
-        print item[0], item[1]
-    print "--------\n"
-
-    # 寻找对应关系
-    y3 = w.model.most_similar([u'质量', u'不错'], [u'电脑桌'], topn=3)
-    for item in y3:
-        print item[0], item[1]
-    print "--------\n"
-
-    # 寻找不合群的词
-    y4 = w.model.doesnt_match(u"书 书籍 教材 很 沙发 电器".split())
-    print u"不合群的词：", y4
-    print "--------\n"
+    # # 寻找对应关系
+    # y3 = w.model.most_similar([u'质量', u'不错'], [u'电脑桌'], topn=3)
+    # for item in y3:
+    #     print item[0], item[1]
+    # print "--------\n"
+    #
+    # # 寻找不合群的词
+    # y4 = w.model.doesnt_match(u"书 书籍 教材 很 沙发 电器".split())
+    # print u"不合群的词：", y4
+    # print "--------\n"
