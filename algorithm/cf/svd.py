dcb802d5ca7e54f56cae5f9a6f5d5f6bcd84d166
wangrui <35493032@qq.com>
2019-05-18 17:29:38 +0800

    RSVD (biasSVD) 完成
    2019-05-18 17:19:51,288 : INFO : 开始读取日志文件，../../dataset/movielens//ml-1m//ratings.dat
    2019-05-18 17:19:56,227 : INFO : 日志文件读取完成
    2019-05-18 17:19:56,317 : INFO : 开始切分训练，测试数据集 9:1
    2019-05-18 17:19:57,173 : INFO : 切分训练，测试数据集完毕,训练数据900189条，测试数据100020条。
    2019-05-18 17:19:57,196 : INFO : 共900189条评分记录，用户共6040个，物品共3706个。
    2019-05-18 17:19:57,290 : INFO : 开始训练，F: 10 ,N:5 ,alpha:0.0200 ,lambda:0.1500。
    2019-05-18 17:19:57,291 : INFO : =====N:0
    2019-05-18 17:20:11,272 : INFO : =====N:1
    2019-05-18 17:20:25,295 : INFO : =====N:2
    2019-05-18 17:20:39,313 : INFO : =====N:3
    2019-05-18 17:20:53,350 : INFO : =====N:4
    2019-05-18 17:21:07,441 : INFO : 训练结束，共耗时70秒。
    2019-05-18 17:21:07,441 : INFO : savedat 开始
    2019-05-18 17:21:08,260 : INFO : [  RSVD]F: 10, N: 5 ,alpha: 0.020000 ,lambda: 0.150000, RMSE: 0.914359

--- algorithm/cf/svd.py
+++ algorithm/cf/svd.py
@@ -153,8 +153,64 @@ class SVD(object):
         return math.sqrt(sum(scores)/len(scores))
 
 
+class RSVD(SVD):
+    """RSVD"""
+    def __init__(self, fi, sep='::', f=F):
+        SVD.__init__(self, fi, sep=sep, f=f)
+        self.mu = np.mean(self.df['rating'])
+        self.bu = {}
+        self.bi = {}
+
+    def splitdata(self,M=10,key=1):
+        SVD.splitdata(self, M=M, key=key)
+        for user in self.users:
+            self.bu[user] = 0
+        for item in self.items:
+            self.bi[item] = 0
+
+    def predict(self, u, i):
+        """
+        预测
+        :param u: user id
+        :param i: item id
+        :return:
+        """
+        return sum(self.P[u][f] * self.Q[i][f] for f in range(self.F))+self.mu+self.bu[u]+self.bi[i]
+
+    def fit(self, N=10, alpha=.1, _lambda=.1, out=1):
+        """
+        训练
+        :param N:    迭代次数，默认10
+        :param alpha:   a 参数，默认
+        :param _lambda: "入"参数 ,默认
+        :param out: 是否保存模型文件 ,默认1，输出
+        :return:
+        """
+        slowRate = 0.90
+        start = time.time()
+        logging.info('开始训练，F: %d ,N:%d ,alpha:%.4f ,lambda:%.4f。' % (self.F, N, alpha, _lambda))
+        for step in range(0, N):
+            logging.info('=====N:%d' % (step,))
+            for row in self.train.itertuples():
+                u, i, rui = row[1], row[2], row[3]
+                pui = self.predict(u, i)
+                eui = rui - pui
+                self.bu[u] +=alpha*(eui-_lambda*self.bu[u])
+                self.bi[i] +=alpha*(eui-_lambda*self.bi[i])
+                for f in range(0, self.F):
+                    self.P[u][f] += alpha * (self.Q[i][f] * eui - _lambda * self.P[u][f])
+                    self.Q[i][f] += alpha * (self.P[u][f] * eui - _lambda * self.Q[i][f])
+            alpha *= slowRate
+        logging.info('训练结束，共耗时%d秒。' % (time.time() - start))
+        if out:
+            # 保存模型文件
+            fname = '%s.%s.%d_%d_%f_%f.class.dat' % (self.file, self.__class__.__name__, self.F, N, alpha, _lambda)
+            self.savedat(fname)
+        return self.P, self.Q
+
+
 if __name__ == '__main__':
-    clsmap = {1:SVD,2:SVD,3:SVD,4:SVD,}
+    clsmap = {1:SVD,2:RSVD,3:SVD,4:SVD,}
     parser = OptionParser()
     parser.add_option('-i', '--in', type=str, help='用户访问日志文件', dest='file')
     parser.add_option('--train', type=int, default=0.9, help='训练数据占比,默认0.9', dest='train')
@@ -162,7 +218,7 @@ if __name__ == '__main__':
     parser.add_option('-n', type=int, default=10, help='迭代次数，默认10', dest='n')
     parser.add_option('-a', type=float, default=.1, help='alpha参数默认.1', dest='a')
     parser.add_option('-l', type=float, default=.1, help='lambda参数默认.1', dest='l')
-    parser.add_option('-m', type=int, default=1, help='模型类型，1-svd,2-biassvd,3-svd++,4-tsvd++,默认1', dest='m')
+    parser.add_option('-m', type=int, default=1, help='模型类型，1-svd,2-rsvd,3-svd++,4-tsvd++,默认1', dest='m')
     parser.add_option('-f', type=int, default=10, help='兴趣因子数，降维后的特征数,默认10', dest='f')
 
     options, args = parser.parse_args()
@@ -180,6 +236,7 @@ if __name__ == '__main__':
     f = options.f
     m = options.m
 
+
     svd = None
     # 如果需要加载已经保存好的模型文件，取消注释
     # fclsname = '../../dataset/movielens/ml-1m/ratings.dat.SVD.10_5_0.011810_0.150000.class.dat'
@@ -190,9 +247,9 @@ if __name__ == '__main__':
     # except Exception as e:
     #     logging.error('%s文件不存在' % (fclsname,))
     if not svd:
-        svd = SVD(fname,sep,f)
+        svd = clsmap[m](fname,sep,f)
         svd.splitdata(M+key,key=key)
         svd.fit(N=N,alpha=alpha,_lambda=_lambda)
     rmse = svd.rmse()
-    logging.info('SVD: F: %d, N: %d ,alpha: %f ,lambda: %f, RMSE: %f' %
-                 (f, N, alpha, _lambda, rmse))
+    logging.info('[%6s]F: %d, N: %d ,alpha: %f ,lambda: %f, RMSE: %f' %
+                 (svd.__class__.__name__, f, N, alpha, _lambda, rmse))
